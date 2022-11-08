4cb45998c9142bca0a68352a4ec8972dc6712e03
Panther Robotics <panthers12241@gmail.com>
2022-11-08 10:05:40 -0700

    FTC2022

--- FTC_APP-2022/road-runner-quickstart-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeep2022FTC.java
+++ FTC_APP-2022/road-runner-quickstart-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeep2022FTC.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -13,15 +14,19 @@ public class MeepMeep2022FTC {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
+                        drive.trajectorySequenceBuilder(new Pose2d(35, -59, Math.toRadians(90)))
+                                //.strafeTo(new Vector2d(35.8,-10.84))
+                                .strafeTo(new Vector2d(10.6,-60.8))
+                                .lineToConstantHeading(new Vector2d(10.4, -24.1))
+
+                                /*.forward(30)
                                 .turn(Math.toRadians(90))
                                 .forward(30)
                                 .turn(Math.toRadians(90))
                                 .forward(30)
                                 .turn(Math.toRadians(90))
                                 .forward(30)
-                                .turn(Math.toRadians(90))
+                                .turn(Math.toRadians(90))*/
                                 .build()
                 );
 
